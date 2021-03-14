c45f273ec706b46079329c757294f40eb12731b9
Alan Rempel <alan.rempel@gmail.com>
2021-03-14 06:25:06 -0400

    Make the IntersectionObserver observe some intersections

--- resources/js/toc.js
+++ resources/js/toc.js
@@ -8,18 +8,29 @@ tocTemplate.innerHTML = `
 </nav>
 `;
 
+const hasAriaCurrent = 'ariaCurrent' in document.createElement('a');
+
+const setAriaCurrent = hasAriaCurrent
+  ? el => el.ariaCurrent = "true"
+  : el => el.setAttribute("aria-current", "true");
+
+const removeAriaCurrent = hasAriaCurrent
+  ? el => el.ariaCurrent = "false"
+  : el => el.removeAttribute("aria-current");
+
 export default class extends HTMLElement {
   constructor() {
     super();
     let shadowRoot = this.attachShadow({mode: 'open'});
     shadowRoot.appendChild(tocTemplate.content);
-    this.visibleHeadings = new Map();
+    this.knownHeadings = new Map();
     this.currentHeading = null;
   }
 
   connectedCallback() {
     let headings = document.getElementById(this.getAttribute('for')).querySelectorAll('h2');
     let nav = this.shadowRoot.lastElementChild;
+    let preceding = null;
     for (let heading of headings) {
       let id;
       if (!heading.id) {
@@ -32,33 +43,37 @@ export default class extends HTMLElement {
       link.href = '#' + id;
       link.innerHTML = heading.innerHTML;
       nav.appendChild(link);
+      let knownHeading = { target: heading, link, preceding };
+      this.knownHeadings.set(heading, knownHeading);
+      preceding = knownHeading;
     }
     if ('IntersectionObserver' in window) {
-      let headingObserverCallback = function (entries, _observer) {
-        let highestIntersectingEntry = entries.reduce(
+      let headingObserverCallback = (entries, _observer) => {
+        let incumbentEntry = null;
+        /* relevantEntry is, if at least one entry is intersecting, the
+           intersecting entry closest to the top of the viewport, otherwise the
+           entry closest to the top of the viewport. */
+        let lookingForIncumbent = true;
+        let relevantEntry = entries.reduce(
           (acc, next) => {
-            if (next.isIntersecting && !acc.isIntersecting) {
+            if (lookingForIncumbent && next === this.currentHeading?.target) {
+              incumbentEntry = next;
+              lookingForIncumbent = false;
+            }
+            if (next.isIntersecting && !acc?.isIntersecting) {
               return next;
-            } else if (next.isIntersecting /* && acc.isIntersecting */) {
-              return next.boundingClientRect.top > acc.boundingClientRect.top ? next : acc;
+            } else if (next.isIntersecting /* && acc && acc.isIntersecting */) {
+              return next.boundingClientRect.top < acc.boundingClientRect.top ? next : acc;
             } else /* if (!next.isIntersecting) */ {
               return acc;
             };
-          });
-        if (highestIntersectingEntry.isIntersecting) {
-          this.setCurrentHeading(highestIntersectingEntry);
-        } else {
-
-        };
-        for (let entry of entries) {
-          if (entry.isIntersecting) {
-            let previousOffset = this.visibleHeadings.get(entry.target);
-            if (previousOffset) {}
-            this.visibleHeadings.set(entry.target, entry.boundingClientRect.top);
-          } else {
-            this.visibleHeadings.delete(entry.target);
-          }
-        }
+          }, null);
+        if (relevantEntry?.isIntersecting) {
+          this.setCurrentHeading(relevantEntry);
+        } else if (incumbentEntry && incumbentEntry.boundingClientRect.top > 0) {
+          // not intersecting, but positive: below the viewport
+          this.setCurrentHeading(this.currentHeading.preceding);
+        }; // otherwise, current heading does not change.
       }
       let headingObserverOptions = {
         threshold: 1.0,
@@ -71,5 +86,12 @@ export default class extends HTMLElement {
     }
   }
 
-  activeHeading
+  setCurrentHeading(newHeadingEntry) {
+    if (this.currentHeading) removeAriaCurrent(this.currentHeading.link);
+    let newCurrentHeading = newHeadingEntry && this.knownHeadings.get(newHeadingEntry.target);
+    if (newCurrentHeading) { setAriaCurrent(newCurrentHeading.link) };
+    this.currentHeading = newCurrentHeading;
+
+  }
+
 }
