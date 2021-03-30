17d9e9dcb88dfadaa60b57eb7852f8d933c4202a
Alan Rempel <alan.rempel@gmail.com>
2021-03-30 13:54:24 -0400

    Remove now-unused JS

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -18,20 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
   setUpControl("colour-scheme", setColourScheme, setValue);
   setUpControl("fonts", loadFancyFonts, (control: HTMLInputElement, value: string) => { value === 'fancy' && (control.checked = true) });
 
-  const setOpen = (event: MediaQueryList | MediaQueryListEvent) => {
-    document.querySelectorAll(".lg\\:open").forEach((element) => {
-      (element as HTMLDetailsElement).open = event.matches;
-    })
-  };
-
-  const isLargeViewport = window.matchMedia('(min-width: 960px)');
-  setOpen(isLargeViewport);
-  isLargeViewport.addEventListener('change', setOpen);
-
   document.addEventListener("click", function(event: MouseEvent) {
     const closestDropdown = (event?.target as Element).closest(".dropdown");
     if (closestDropdown) return;
-    const selector = isLargeViewport.matches ? ".dropdown[open]:not(.lg\\:open)" : ".dropdown[open]";
+    const selector = ".dropdown[open]";
     for (const dropdown of document.querySelectorAll(selector)) {
       (dropdown as HTMLDetailsElement).open = false;
     }
