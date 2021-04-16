0ea2217fda506882adc31ac3314d7cdb1af51d12
Alan Rempel <alan.rempel@gmail.com>
2021-04-16 19:34:43 -0400

    I forgot another class

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,17 @@
 const public = process.env.PUBLIC_DIR;
 
+const additionalCSSclasses = [
+  'colour-scheme-auto', 'colour-scheme-light', 'colour-scheme-dark',
+  'visible', 'invisible',
+  'fonts-default',
+];
+
 module.exports = {
   plugins: [
     require('@fullhuman/postcss-purgecss')({
       content: [`./${public}/**/*.html`],
       extractors: [ { extractor: require('purgecss-from-html'), extensions: ['html'] } ],
-      safelist: ['colour-scheme-auto', 'colour-scheme-light', 'colour-scheme-dark', 'visible', 'invisible'],
+      safelist: additionalCSSclasses,
     }),
     require('cssnano')({ preset: 'default' }),
     require('autoprefixer')({ flexbox: false }),
