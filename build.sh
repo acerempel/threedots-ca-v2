2069c0bf16c62121b4e84d77ca332e50a45bc96c
Alan Rempel <alan.rempel@gmail.com>
2021-04-16 14:42:42 -0400

    Use PurgeCSS

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 yarn run sass resources/sass/main.scss public/css/main.css --embed-sources
-yarn run postcss public/css/main.css --replace --map --env production
 yarn run rollup -c
 php please ssg:generate
+yarn run postcss $PUBLIC_DIR/css/main.css --replace --map --env production
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
+const public = process.env.PUBLIC_DIR;
+
 module.exports = {
   plugins: [
+    require('@fullhuman/postcss-purgecss')({
+      content: [`./${public}/**/*.html`],
+      extractors: [ { extractor: require('purgecss-from-html'), extensions: ['html'] } ]
+    }),
     require('cssnano')({ preset: 'default' }),
     require('autoprefixer')({ flexbox: false }),
   ],
