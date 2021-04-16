b8b2a6da3a7f9e9d30aa8a0e7daf53b17cf3685f
Alan Rempel <alan.rempel@gmail.com>
2021-04-16 16:38:58 -0400

    Fix problems

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,8 @@ module.exports = {
   plugins: [
     require('@fullhuman/postcss-purgecss')({
       content: [`./${public}/**/*.html`],
-      extractors: [ { extractor: require('purgecss-from-html'), extensions: ['html'] } ]
+      extractors: [ { extractor: require('purgecss-from-html'), extensions: ['html'] } ],
+      safelist: ['colour-scheme-auto', 'colour-scheme-light', 'colour-scheme-dark', 'visible', 'invisible'],
     }),
     require('cssnano')({ preset: 'default' }),
     require('autoprefixer')({ flexbox: false }),
