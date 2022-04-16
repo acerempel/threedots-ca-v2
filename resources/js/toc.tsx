d47c996a304caad358a41fcc2c72ff6b33d8cda9
Alan Rempel <alan.rempel@gmail.com>
2022-04-16 11:41:32 -0400

    Fix toc

--- resources/js/toc.tsx
+++ resources/js/toc.tsx
@@ -3,4 +3,5 @@ import { render } from 'solid-js/web';
 import { TOC } from './toc/view';
 
 const elem = document.getElementById('all-posts');
-if (elem) render(() => TOC(elem), document.getElementById('toc')!);
+const toc = document.getElementById('toc');
+if (toc && elem) render(() => <TOC forElem={elem} />, toc);
