463de5af1551a4c6018efd8f414f467239245f53
Alan Rempel <alan.rempel@gmail.com>
2022-04-22 13:43:20 -0400

    Use Solid to render comments

--- resources/js/comments.ts
+++ resources/js/comments.ts
@@ -1,50 +1,10 @@
-const load_comments = async (comments_list: HTMLElement) => {
-  const url = `https://comments.threedots.ca/comments?page_url=${window.location.pathname}`
-    try {
-    const response = await fetch(url, {
-      headers: {'Accept': 'application/json'},
-    })
-    const data = await response.json()
-    display_comments(comments_list, data)
-  } catch (err) {
-    console.error(`Fetching comments failed: ${err}`)
-    comments_list.innerHTML = `<p>There was a problem fetching comments!</p>`
-  }
-}
-
-interface Comment {
-  author: String,
-  date: String,
-  content: String,
-}
-
-const display_comments = (comments_list: HTMLElement, data: Comment[]) => {
-  if (data.length === 0) {
-    comments_list.innerHTML = "<p>No comments yet!</p>"
-  } else {
-    const template = document.getElementById('comment-template')! as HTMLTemplateElement
-    const fragment = document.createDocumentFragment()
-    for (const comment of data) {
-      const container = document.createElement('div')
-      container.innerHTML = `
-      <span slot="author">${comment.author}</span>
-      <span slot="date">${comment.date}</span>
-      ${comment.content}
-      `
-      const shadow = container.attachShadow({ mode: 'open' })
-      shadow.appendChild(template.content.cloneNode(true))
-      fragment.appendChild(container)
-    }
-    comments_list.appendChild(fragment)
-  }
-  comments_list.appendChild((document.getElementById('comment-form-template') as HTMLTemplateElement).content)
-}
+import {display_comments} from "./comments_view"
 
 const observer_callback = (list: HTMLElement): IntersectionObserverCallback => {
   return (entries, observer) => {
     for (const entry of entries) {
       if (entry.isIntersecting) {
-        load_comments(list)
+        display_comments(list)
         observer.unobserve(entry.target)
       }
     }
