0456e483beb7deef37ee61a57481e878bf601fae
Alan Rempel <alan.rempel@gmail.com>
2022-06-20 18:33:11 -0400

    Remove comments

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,36 +1,17 @@
 import { setUpControl, setColourScheme } from './colour-scheme';
-import { enable_comments, disable_comments } from "./comments";
 
 document.addEventListener('DOMContentLoaded', async () => {
   const setValue = (control: HTMLInputElement, val: string) => { control.value = val };
   const loadFancyFonts = (val: string) => {
-    const submit = document.getElementById('comment-submit')
     if (val === 'fancy') {
       document.body.classList.remove('fonts-default');
-      if (submit) {
-        submit.style.setProperty('position', 'relative')
-        submit.style.setProperty('top', '1px')
-      }
     } else {
       document.body.classList.add('fonts-default');
-      if (submit) {
-        submit.style.removeProperty('position')
-        submit.style.removeProperty('top')
-      }
     }
   };
 
-  const setCommentsEnabled = (state: string) => {
-        if (state === 'on') {
-            enable_comments()
-        } else {
-            disable_comments()
-        }
-  }
-
   setUpControl("colour-scheme", setColourScheme, setValue);
   setUpControl("fonts", loadFancyFonts, setValue);
-  setUpControl("comments-option", setCommentsEnabled, (control, value) => { control.checked = value === 'on' })
 
   const elem = document.getElementById('all-posts');
   const toc = document.getElementById('toc');
